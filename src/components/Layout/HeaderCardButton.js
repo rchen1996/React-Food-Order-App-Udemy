a62d8cc5250c7b69ce03cc5b0543493d50d93577
Rebecca Chen <rebecca061996@gmail.com>
2021-06-03 10:20:31 -0400

    use context to update cart badge displaying number of items in cart

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,14 +1,23 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
+
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCardButton.module.css';
 
 const HeaderCardButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((currentAmt, item) => {
+    return currentAmt + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{props.cartItems}</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
